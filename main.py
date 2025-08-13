a9405498c4342d3117ed6d75d101e34bea6a5532
guilhermefiap <guipelissari2000@gmail.com>
2025-08-13 19:44:21 -0300

    Adicionar conexão com banco de dado e Implementar função para buscar contatos no Supabase

--- main.py
+++ main.py
@@ -10,3 +10,15 @@ SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 ZAPI_INSTANCE_ID = os.getenv("ZAPI_INSTANCE_ID")
 ZAPI_TOKEN = os.getenv("ZAPI_TOKEN")
 ZAPI_CLIENT_TOKEN = os.getenv("ZAPI_CLIENT_TOKEN")
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+def buscar_contatos():
+    try:
+        response = supabase.table("contatos").select("*").limit(3).execute()
+        return response.data
+    except Exception as e:
+        print(f"Erro ao buscar contatos: {e}")
+        return []
+
+
